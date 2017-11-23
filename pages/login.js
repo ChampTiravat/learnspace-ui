4bf5634683775749577cedecf24a5e22cc7e2601
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-23 21:40:33 +0700

    Using <Card /> as a login panel

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import { UnAuthenticatedLayout } from "../components/Layout";
+import Card, { Header, Body, Footer } from "../components/Card";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <UnAuthenticatedLayout>
-        <h1>Login page</h1>
+        <Card>
+          <Header>ยืนยันตัวตนเพื่อเข้าสู่ระบบ</Header>
+          <Body>asdasdddddddddddddddd</Body>
+          <Footer>
+            <button>ปิด</button>
+          </Footer>
+        </Card>
       </UnAuthenticatedLayout>
     );
   }
