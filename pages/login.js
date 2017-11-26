b9eb305f845e72c4cae8c957e9ac142fdb16a5bf
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-26 16:56:00 +0700

    Restrucuted login component

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 import UnAuthenticatedLayout from "../core/components/UnAuthenticatedLayout";
+import NavigationBar from "../core/components/NavigationBar";
+import Container from "../core/components/Container";
+import Wallpaper from "../core/components/Wallpaper";
 import LoginCard from "../core/components/LoginCard";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <UnAuthenticatedLayout>
-        <LoginCard />
+        <Wallpaper pageName="login">
+          <NavigationBar />
+          <Container transparent>
+            <LoginCard />
+          </Container>
+        </Wallpaper>
       </UnAuthenticatedLayout>
     );
   }
