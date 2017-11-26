2c3a5bb7246852ef1d2f3b29f93d8e053a8fd586
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-27 04:44:57 +0700

    Created  a central URI endpoints of the UI

---
+++ core/constants/endpoints/ui.js
@@ -0,0 +1,15 @@
+/**
+ * @name UI
+ * @desc Provide URI path of the UI
+ */
+export default {
+  LANDING_PAGE: ``,
+  HOME_PAGE: `/`,
+  LOGIN_PAGE: `/login`,
+  REGISTER_PAGE: `/register`,
+  DASHBOARD_PAGE: `/dashboard`,
+  POST_PAGE: `/post`,
+  PROFILE_PAGE: `/profile`,
+  CLASSROOM_PAGE: `/classroom`,
+  CLASSROOM_CHAT_PAGE: `/classroom/chat/`
+};
