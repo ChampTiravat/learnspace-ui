839a120cdfda87b4cf4d409af3eca9a0d6310402
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-27 02:14:20 +0700

    Added more theme properties

--- core/themes/default.js
+++ core/themes/default.js
@@ -2,5 +2,7 @@ export default {
   primaryColor: "#3b90f9",
   fontColorLight: "#fff",
   backgroundColor: "#f5f5f5",
-  textColor: "#777"
+  textColor: "#777",
+  textInputBackgroundColor: "#F3F3F3",
+  borderRadius: "5px"
 };
