90e730ff535be98e3d1a0e5151ede631ec306947
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-27 00:42:37 +0700

    Create a redux form's reducer

--- core/reducers/index.js
+++ core/reducers/index.js
@@ -1,6 +1,10 @@
+import { reducer as formReducer } from "redux-form";
+
 import user from "./user";
 
 // import other reducers into the object below
 export default {
-  user
+  user,
+  form: formReducer,
+  test: () => "hello"
 };
