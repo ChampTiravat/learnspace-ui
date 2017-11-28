0aa27fcd2f2352b190338891b1f5e8faa1d219b5
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-28 18:57:46 +0700

    :lipsick: Change BG color

--- core/components/NavigationBar.js
+++ core/components/NavigationBar.js
@@ -107,7 +107,7 @@ const SearchBar = styled.input`
   font-family: "Arial";
   font-weight: 300;
   color: ${p => p.theme.textColor};
-  background-color: #f3f3f3;
+  background-color: #fff;
 `;
 
 const SearchButton = styled.button`
