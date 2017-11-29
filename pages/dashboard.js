557b6773b9e5fd9fcb1113dff10e90b9a18140c0
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-29 11:30:27 +0700

    :hammer: Adjusted a style of dashboard heading panel

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -10,64 +10,62 @@ import Wallpaper from "../core/components/Wallpaper";
 import ActivityPanel from "../core/components/ActivityPanel";
 import NotificationPanel from "../core/components/NotificationPanel";
 import ClassroomsList from "../core/components/ClassroomsList";
+import Panel from "../core/components/Panel";
 import withData from "../core/withData";
 
-const Panel = styled.div`
+const DashboardPageHeading = styled.div`
+  width: 550px;
   display: inline-block;
-  width: ${p => p.width}%;
-  height: 100%;
+  text-align: left;
+  float: right;
+  margin-top: 2.9em;
+  margin-right: 1em;
+  margin-left: 0;
+  h2 {
+    font-weight: 400;
+    font-size: 1.7em;
+    color: ${p => p.theme.primaryColor};
+    margin-bottom: 0;
+  }
+  p {
+    margin-top: 0;
+    color: #777;
+    font-size: 1.1em;
+  }
+`;
+
+const DashboardPageHeadingIcon = styled.img`
+  margin-right: 0;
+  width: 110px;
 `;
 
 const PageHeadingPanel = ({ child }) => (
-  <Card fluidWidth textCenter padding="2em">
-    <div
-      style={{
-        width: "100px",
-        height: "100px",
-        backgroundColor: "#333",
-        display: "inline-block",
-        marginRight: "1em"
-      }}
-    />
-    <div
-      style={{
-        width: "500px",
-        display: "inline-block",
-        textAlign: "left"
-      }}
-    >
+  <Card fluidWidth textCenter padding="1.5em 1em">
+    <DashboardPageHeadingIcon src="/static/images/mascots/charcter.svg" />
+    <DashboardPageHeading>
       <h2>คุณสามารถดูการแจ้งเตือนและสิ่งที่เกิดขึ้นได้ที่นี่</h2>
       <p>ศูนย์รวมการแจ้งเตือนต่างๆของห้องเรียนที่คุณได้สร้างหรือเข้าร่วม</p>
-    </div>
+    </DashboardPageHeading>
   </Card>
 );
 
-class DashboardPage extends React.Component {
-  render() {
-    return [
-      <Head>
-        <title>หน้าหลัก | LEARNSPACE</title>
-      </Head>,
-      <AuthenticatedLayout>
-        <NavigationBar isLoggedIn={true} />
-        <Container>
-          <Panel
-            width="40"
-            style={{
-              float: "left"
-            }}
-          >
-            <NotificationPanel />
-          </Panel>
-          <Panel width="60">
-            <PageHeadingPanel />
-            <ClassroomsList />
-          </Panel>
-          <ActivityPanel />
-        </Container>
-      </AuthenticatedLayout>
-    ];
-  }
-}
+const DashboardPage = () => [
+  <Head>
+    <title>หน้าหลัก | LEARNSPACE</title>
+  </Head>,
+  <AuthenticatedLayout>
+    <NavigationBar isLoggedIn={true} />
+    <Container>
+      <Panel left width="40">
+        <NotificationPanel />
+      </Panel>
+      <Panel right width="60">
+        <PageHeadingPanel />
+        <ClassroomsList />
+      </Panel>
+      <ActivityPanel />
+    </Container>
+  </AuthenticatedLayout>
+];
 
 export default withData(DashboardPage);
