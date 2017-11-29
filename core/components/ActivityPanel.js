2cf1451f70b2a29afae35786fe2800aa95e83e60
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-29 08:03:32 +0700

    :sparkles: Initialized Activity Panel

---
+++ core/components/ActivityPanel.js
@@ -0,0 +1,20 @@
+import styled from "react";
+
+/**
+ * @name ActivityPanel
+ * @desc Display a menu of each page at the bottom of the screen
+ */
+const ActivityPanel = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 900px;
+  height: 60px;
+  background-color: #fff;
+  box-shadow: 0 -5px 20px rgba(0, 0, 0, 0.15);
+  border-top-left-radius: 7px;
+  border-top-right-radius: 7px;
+`;
+
+export default ActivityPanel;
