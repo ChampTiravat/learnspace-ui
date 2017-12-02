ab030fa2537948d32de3724c07eb0e62ffd5f360
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-02 16:22:57 +0700

    :sparkles: Added a parameterized URI for classroom page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const next = require("next");
+const url = require("url");
+const pathMatch = require("path-match");
 
 require("dotenv").config();
 
@@ -7,13 +9,22 @@ const app = next({
   dev: process.env.NODE_ENV !== "production" ? true : false
 });
 const handle = app.getRequestHandler();
+const route = pathMatch();
+const matchURIScheme = route("/classroom/:id");
 
 app.prepare().then(() => {
   const server = express();
   const PORT = process.env.APP_PORT || 3000;
 
   server.get("*", (req, res) => {
-    return handle(req, res);
+    const { pathname, query } = url.parse(req.url, true);
+    const params = matchURIScheme(pathname);
+
+    if (!params) {
+      return handle(req, res);
+    }
+
+    return app.render(req, res, "/classroom/", Object.assign(params, query));
   });
 
   server.listen(PORT, err => {
