2c67b3fca29e4283f38e72f41e29c295c1710e82
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-06 21:50:45 +0700

    :sparkles: Created a Chatroom modal

---
+++ core/components/ChatroomModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+import Modal from "./Modal";
+import { Header, Body } from "./Card";
+
+const ChatroomModal = () => (
+  <Modal large>
+    <Header>แชทรวม</Header>
+    <Body>asdsd</Body>
+  </Modal>
+);
+
+export default ChatroomModal;
