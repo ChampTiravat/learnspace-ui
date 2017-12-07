22881d2a8d63ad78be6438ed96d81d579c70612b
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-07 22:41:28 +0700

    :sparkles: Moved chat messages panel to a seperated panel

--- pages/chatroom.js
+++ pages/chatroom.js
@@ -2,15 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import Head from "next/head";
 
-import Card, { Header, Body, Footer } from "../core/components/Card";
-import { Form, TextArea } from "../core/components/Form";
-import { PrimaryButton } from "../core/components/Button";
 import AuthenticatedLayout from "../core/components/AuthenticatedLayout";
 import NavigationBar from "../core/components/NavigationBar";
 import Container from "../core/components/Container";
 import Panel from "../core/components/Panel";
 import UserConnectionStatusPanel from "../core/components/UserConnectionStatusPanel";
-import ChatMessage from "../core/components/ChatMessage";
+import ChatMessagesPanel from "../core/components/ChatMessagesPanel";
 import ActivityPanel from "../core/components/ActivityPanel";
 
 const ChatroomModal = () => [
@@ -24,47 +21,7 @@ const ChatroomModal = () => [
         <UserConnectionStatusPanel />
       </Panel>
       <Panel right width="60">
-        <Card
-          style={{
-            height: "auto",
-            maxWidth: "100%",
-            width: "1000px"
-          }}
-        >
-          <Header>ห้องแชท</Header>
-          <Body style={{ overflowY: "scroll", height: "550px" }}>
-            {[0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0].map(
-              (n, i) =>
-                n === 1 ? (
-                  <div style={{ textAlign: "right" }}>
-                    <ChatMessage
-                      key={i}
-                      creator="tiravat"
-                      content="ฟหกฟหกฟหกฟหกฟหกฟหมกนาฟดรกหาดสฟมดยำดใยฟหกใดยสกฟหยกสฟหดสกฟหยดสกหฟดสยกหสดยกหสฟดยสกหยดสหยฟสดยกหสดยสกหฟยดสกหยฟดสกหยฟดสกหยดสหกด"
-                    />
-                  </div>
-                ) : (
-                  <div style={{ textAlign: "left" }}>
-                    <ChatMessage
-                      key={i}
-                      creator="tiravat"
-                      content="ฟหกฟหกฟหกฟหกฟหกฟหมกนาฟดรกหาดสฟมดยำดใยฟหกใดยสกฟหยกสฟหดสกฟหยดสกหฟดสยกหสดยกหสฟดยสกหยดสหยฟสดยกหสดยสกหฟยดสกหยฟดสกหยฟดสกหยดสหกด"
-                    />
-                  </div>
-                )
-            )}
-          </Body>
-          <Footer>
-            <Form>
-              <TextArea
-                style={{ marginBottom: "1em", width: "100%", height: "50px" }}
-              />
-              <PrimaryButton textCenter fluidWidth>
-                ส่ง
-              </PrimaryButton>
-            </Form>
-          </Footer>
-        </Card>
+        <ChatMessagesPanel />
       </Panel>
       <ActivityPanel />
     </Container>
