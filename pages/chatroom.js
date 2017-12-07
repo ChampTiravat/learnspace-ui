a96344b9c9556a84ee0306c84acbae65394a8c87
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-07 22:17:15 +0700

    :sparkles: Moved chat message to a seperated component

--- pages/chatroom.js
+++ pages/chatroom.js
@@ -10,60 +10,8 @@ import NavigationBar from "../core/components/NavigationBar";
 import Container from "../core/components/Container";
 import Panel from "../core/components/Panel";
 import UserConnectionStatusPanel from "../core/components/UserConnectionStatusPanel";
+import ChatMessage from "../core/components/ChatMessage";
 import ActivityPanel from "../core/components/ActivityPanel";
-
-const NotificationPanelCard = Card.extend`
-  height: 700px;
-  padding-bottom: 2em;
-`;
-
-const NotificationOriginPic = styled.img`
-  display: inline-block;
-  float: left;
-  width: 60px;
-  height: 60px;
-  border: none;
-  border-radius: 50%;
-  background-color: #ccc;
-  margin-right: 1.5em;
-  margin-top: 0.5em;
-`;
-
-const NotificationInfo = styled.div`
-  padding-left: 5em;
-  h4 {
-    color: ${p => p.theme.primaryColor};
-    font-weight: 500;
-    font-size: 1.1em;
-    margin-bottom: 0;
-  }
-  p {
-    margin: 0;
-    color: #777;
-    font-family: arial;
-  }
-`;
-
-const NotificationCard = Card.extend`
-  display: inline-block;
-  box-shadow: none;
-  padding: 1em 1em 1.8em 1em;
-  max-width: 450px;
-  margin-bottom: 1em;
-  &:hover {
-    background: #f5f5f5;
-  }
-`;
-
-const NotificationItem = ({ origin, content }) => (
-  <NotificationCard>
-    <NotificationOriginPic />
-    <NotificationInfo>
-      <h4>{origin}</h4>
-      <p>{content}</p>
-    </NotificationInfo>
-  </NotificationCard>
-);
 
 const ChatroomModal = () => [
   <Head>
@@ -89,17 +37,17 @@ const ChatroomModal = () => [
               (n, i) =>
                 n === 1 ? (
                   <div style={{ textAlign: "right" }}>
-                    <NotificationItem
+                    <ChatMessage
                       key={i}
-                      origin="tiravat"
+                      creator="tiravat"
                       content="ฟหกฟหกฟหกฟหกฟหกฟหมกนาฟดรกหาดสฟมดยำดใยฟหกใดยสกฟหยกสฟหดสกฟหยดสกหฟดสยกหสดยกหสฟดยสกหยดสหยฟสดยกหสดยสกหฟยดสกหยฟดสกหยฟดสกหยดสหกด"
                     />
                   </div>
                 ) : (
                   <div style={{ textAlign: "left" }}>
-                    <NotificationItem
+                    <ChatMessage
                       key={i}
-                      origin="tiravat"
+                      creator="tiravat"
                       content="ฟหกฟหกฟหกฟหกฟหกฟหมกนาฟดรกหาดสฟมดยำดใยฟหกใดยสกฟหยกสฟหดสกฟหยดสกหฟดสยกหสดยกหสฟดยสกหยดสหยฟสดยกหสดยสกหฟยดสกหยฟดสกหยฟดสกหยดสหกด"
                     />
                   </div>
