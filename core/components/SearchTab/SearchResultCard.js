fbfbd62aa63301dee5c362cd3b99cd1c887554b4
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-13 20:22:06 +0700

    :sparkles: Added a search result items mockup

---
+++ core/components/SearchTab/SearchResultCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import styled from "styled-components";
+
+import Card, { Footer, Body } from "../Card";
+
+/**
+ *  @name SearchResultCardWrapper
+ *  @desc A presentational component for Navbar search result panel
+ *  @param show : the state of navbar search result panel apearence
+ */
+const SearchResultCardWrapper = Card.extend`
+  display: ${p => (p.show ? "block" : "none")};
+  position: absolute;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  padding: 1em 0.5em;
+  height: 300px;
+  width: 400px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  margin: 0 0 0 0.5em;
+`;
+
+/**
+ *  @name MoreSearchResultLink
+ *  @desc A button links to full search result(s) page
+ */
+const MoreSearchResultLink = styled.a`
+  color: ${p => p.theme.primaryColor};
+  font-size: 1em;
+  font-weight: 400;
+  cursor: pointer;
+`;
+
+/**
+ *  @name SearchResultCard
+ *  @desc Render the search result panel dialog
+ *  @param children : Contents of search result items
+ *  @param show : the state of navbar search result panel apearence
+ */
+const SearchResultCard = ({ children, show }) => (
+  <SearchResultCardWrapper show={show}>
+    <Body height="230px" overflowY="scroll">
+      {children}
+    </Body>
+    <Footer textCenter>
+      <Link href="/">
+        <MoreSearchResultLink>ดูผลการค้นหาทั้งหมด</MoreSearchResultLink>
+      </Link>
+    </Footer>
+  </SearchResultCardWrapper>
+);
+
+SearchResultCard.propTypes = {
+  children: PropTypes.array,
+  show: PropTypes.bool.isRequired
+};
+
+SearchResultCardWrapper.propTypes = {
+  show: PropTypes.bool.isRequired
+};
+
+export default SearchResultCard;
---
+++ core/components/SearchTab/SearchResultItem.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+/**
+ *  @name SearchResultItemWrapper
+ *  @desc Wrap Profile picture and the name of a particular search result
+ *  @param show : the state of navbar search result panel apearence
+ */
+const SearchResultItemWrapper = styled.div`
+  padding: 1em 0;
+  color: ${p => p.theme.primaryColor};
+  font-weight: 400;
+  font-size: 1.2em;
+  cursor: pointer;
+  &:hover {
+    background-color: #eee;
+  }
+`;
+
+/**
+ *  @name SearchResultProfilePic
+ *  @desc Display the profile picture of the search result(s)
+ *  @param show : the state of navbar search result panel apearence
+ */
+const SearchResultProfilePic = styled.div`
+  padding: 1.2em;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #c8c8c8;
+  display: inline-block;
+  float: left;
+  margin-top: 0;
+  margin-bottom: 1em;
+  margin-right: 1em;
+`;
+
+/**
+ *  @name SearchResultPanel
+ *  @desc Render to single search item
+ *  @param show : the state of navbar search result panel apearence
+ */
+const SearchResultItem = ({ name }) => (
+  <SearchResultItemWrapper>
+    <SearchResultProfilePic />
+    {name}
+  </SearchResultItemWrapper>
+);
+
+SearchResultItem.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+export default SearchResultItem;
