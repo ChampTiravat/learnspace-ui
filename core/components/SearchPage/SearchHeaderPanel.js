91a336992b575bb0b55d647940ccbde3436422eb
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-17 19:26:12 +0700

    :bug: Bug fixed! Created a required components

--- core/components/SearchPage/SearchHeaderPanel.js
+++ core/components/SearchPage/SearchHeaderPanel.js
@@ -2,12 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { ClassroomHeaderPanel, ClassroomNameHeading } from "../ClassroomPage";
+import Card from "../Card";
 
 const SearchHeaderPanelHeading = styled.h1`
   color: #777;
   font-size: 1.2em;
   font-weight: 400;
+`;
+
+const SearchHeaderPanelWrapper = Card.extend`
+  position: absolute;
+  left: 0;
+  max-width: 100%;
+  padding: 2em;
+  text-align: center;
+`;
+
+const SearchQuery = styled.h1`
+  color: ${p => p.theme.primaryColor};
+  font-size: 1.7em;
+  font-weight: 400;
+  cursor: pointer;
 `;
 
 /**
@@ -16,10 +31,10 @@ const SearchHeaderPanelHeading = styled.h1`
  * @prop searchQuery : User's search text query
  */
 const SearchHeaderPanel = ({ searchQuery }) => (
-  <ClassroomHeaderPanel>
+  <SearchHeaderPanelWrapper>
     <SearchHeaderPanelHeading>ผลการค้นหาสำหรับ</SearchHeaderPanelHeading>
-    <ClassroomNameHeading>"{searchQuery}"</ClassroomNameHeading>
-  </ClassroomHeaderPanel>
+    <SearchQuery>"{searchQuery}"</SearchQuery>
+  </SearchHeaderPanelWrapper>
 );
 
 SearchHeaderPanel.propTypes = {
