f1cc5b3ba3cafe847b71e337581ae4f4a8d47c23
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-17 18:23:22 +0700

    :fire: :hammer: Removed useless codes and use the proper components

--- pages/classroom_join_request.js
+++ pages/classroom_join_request.js
@@ -1,86 +1,40 @@
 import React from "react";
-import Link from "next/link";
 import Head from "next/head";
 import Router from "next/router";
-import styled from "styled-components";
 
+import ClassroomActivityPanel from "../core/components/ClassroomPage/ClassroomActivityPanel";
 import JoinRequestPanel from "../core/components/ClassroomJoinRequestPage/JoinRequestPanel";
+import ClassroomHeaderPanel from "../core/components/ClassroomPage/ClassroomHeaderPanel";
 import AuthenticatedLayout from "../core/components/AuthenticatedLayout";
-import ProfileInfoPanel from "../core/components/ProfileInfoPanel";
-import ClassroomPreview from "../core/components/ClassroomPreview";
-import ClassroomsList from "../core/components/ClassroomsList";
-import Card, { Header, Body } from "../core/components/Card";
-import ActivityPanel from "../core/components/ActivityPanel";
-import { PrimaryButton } from "../core/components/Button";
+import { CLASSROOM_PAGE } from "../core/constants/endpoints/ui";
 import Container from "../core/components/Container";
-import PostsList from "../core/components/PostsList";
-import Panel from "../core/components/Panel";
 import withData from "../core/withData";
-import {
-  ClassroomHeaderPanel,
-  ClassroomNameHeading,
-  Seperator,
-  ClassroomCourseInfoContainer,
-  ClassroomBreifInfo
-} from "../core/components/ClassroomPage";
-import {
-  CREATE_POST_PAGE,
-  EDIT_CLASSROOM_PAGE,
-  CLASSROOM_CHATROOM_PAGE,
-  CLASSROOM_PAGE
-} from "../core/constants/endpoints/ui";
 
 class ClassroomJoinRequestPage extends React.Component {
-  state = {
-    isMember: 1
-  };
-
   static async getInitialProps({ query: { id } }) {
-    return { classId: id, className: "Introduction to Computer Science" };
-  }
-
-  componentDidMount() {
-    console.log(this.props.classId);
+    return {
+      isMember: 1,
+      classroomID: id,
+      classroomName: "Introduction to Computer Science"
+    };
   }
 
   render() {
-    const { isMember } = this.state;
+    const { isMember, classroomID, classroomName } = this.props;
     return !isMember
       ? Router.push(CLASSROOM_PAGE)
       : [
           <Head>
-            <title>{this.props.className} | LEARNSPACE</title>
+            <title>คำขอเข้าร่วม - {this.props.className} | LEARNSPACE</title>
           </Head>,
           <AuthenticatedLayout>
-            <ClassroomHeaderPanel>
-              <ClassroomNameHeading>
-                {this.props.className}
-              </ClassroomNameHeading>
-              <Seperator />
-              <ClassroomBreifInfo>
-                จำนวนสมาชิก <span>30</span> โพสทั้งหมด <span>10</span> การบ้าน{" "}
-                <span>5</span> การสอบ <span>2</span>
-              </ClassroomBreifInfo>
-            </ClassroomHeaderPanel>
-            <Container style={{ marginTop: "10em" }}>
+            <ClassroomHeaderPanel
+              classroomID={classroomID}
+              classroomName={classroomName}
+            />
+            <Container marginTop="10em">
               <JoinRequestPanel />
-              <ActivityPanel>
-                <Link href={CREATE_POST_PAGE} prefetch>
-                  <a>สร้างโพสใหม่</a>
-                </Link>
-                <Link>
-                  <a>เพิ่มการสอบ</a>
-                </Link>
-                <Link href={EDIT_CLASSROOM_PAGE} prefetch>
-                  <a>แก้ไขข้อมูลห้องเรียน</a>
-                </Link>
-                <Link>
-                  <a>สมาชิก</a>
-                </Link>
-                <Link>
-                  <a>คำขอเข้าร่วม</a>
-                </Link>
-              </ActivityPanel>
+              <ClassroomActivityPanel />
             </Container>
           </AuthenticatedLayout>
         ];
