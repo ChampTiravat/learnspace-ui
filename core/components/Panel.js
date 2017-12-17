a493c6a64be8af2dbd822f520886abe749b8e753
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-17 17:21:21 +0700

    :sparkles: Added a box-model control properties

--- core/components/Panel.js
+++ core/components/Panel.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
+/**
+ * @name Panel
+ * @desc Seperate a layout into 2 parts
+ * @prop width(%)
+ * @prop height
+ * @prop padding
+ * @prop paddingTop
+ * @prop paddingBottom
+ * @prop paddingLeft
+ * @prop paddingRight
+ * @prop left
+ * @prop right
+ */
 const Panel = styled.div`
   display: inline-block;
   width: ${p => p.width}%;
-  height: 100%;
+  height: ${p => p.height || "100%"};
   padding: ${p => p.padding || "0"};
   padding-top: ${p => p.paddingTop || "0"};
   padding-bottom: ${p => p.paddingBottom || "0"};
