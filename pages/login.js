5cb324885176a983e51836ea485b56b76a17a8c9
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-17 19:12:05 +0700

    :lipstick: For the beauty :)

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import UnAuthenticatedLayout from "../core/components/UnAuthenticatedLayout";
+import LoginCard from "../core/components/LoginPage/LoginCard";
 import Container from "../core/components/Container";
 import Wallpaper from "../core/components/Wallpaper";
-import LoginCard from "../core/components/LoginPage/LoginCard";
 import withData from "../core/withData";
 
 class LoginPage extends React.Component {
