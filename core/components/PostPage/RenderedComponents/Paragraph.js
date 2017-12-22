adab00777f4e94a2bdd8e90da127ac5847c7e716
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-23 00:24:06 +0700

    :sparkles: Created paragraph component

---
+++ core/components/PostPage/RenderedComponents/Paragraph.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  color: #777;
+  font-size: 1em;
+  font-wight: 400;
+  font-family: "Calibri";
+`;
+
+export default Paragraph;
