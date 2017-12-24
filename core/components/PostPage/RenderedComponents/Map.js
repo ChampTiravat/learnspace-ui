79166b90e3016f8c2195ec16755a65d768ffce8b
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-24 16:45:22 +0700

    :lipstick: shadow added

--- core/components/PostPage/RenderedComponents/Map.js
+++ core/components/PostPage/RenderedComponents/Map.js
@@ -2,13 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const MapWrapper = styled.div`
-      height: 500px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      background-color: #ddd;
-    }
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ddd;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
 `;
 
 /**
