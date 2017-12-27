65a8ef2f941e8a523226458542700077ef26d5a8
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-12-28 00:29:02 +0700

    :fire: Remove URI parameter in classroom page

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const next = require("next");
-const url = require("url");
-const pathMatch = require("path-match");
 
 require("dotenv").config();
 
@@ -9,22 +7,13 @@ const app = next({
   dev: process.env.NODE_ENV !== "production" ? true : false
 });
 const handle = app.getRequestHandler();
-const route = pathMatch();
-const matchURIScheme = route("/classroom/:id");
 
 app.prepare().then(() => {
   const server = express();
   const PORT = process.env.APP_PORT || 3000;
 
   server.get("*", (req, res) => {
-    const { pathname, query } = url.parse(req.url, true);
-    const params = matchURIScheme(pathname);
-
-    if (!params) {
-      return handle(req, res);
-    }
-
-    return app.render(req, res, "/classroom/", Object.assign(params, query));
+    return handle(req, res);
   });
 
   server.listen(PORT, err => {
