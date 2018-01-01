07092fa6076259bd28f9e48c320f227f2ba27b92
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-01 09:52:55 +0700

    :hammer: Remove redux binding from presentational component

--- core/components/PostPage/PostViewer.js
+++ core/components/PostPage/PostViewer.js
@@ -1,5 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
+import { showPostRemovalConfirmationModal } from "../../actions/post-page-actions";
 import PostRemovalConfirmationModal from "./PostRemovalConfirmationModal";
 import PostCommentsPanel from "./PostCommentsPanel";
 import CommentMessageBox from "./CommentMessageBox";
@@ -11,14 +14,22 @@ import Container from "../Container";
  * @name PostViewer
  * @desc Display post content, comments in one component
  */
-const PostViewer = () => (
+const PostViewer = ({ showPostRMModal }) => (
   <Container marginTop="11.5em">
     <PostContent />
     <PostCommentsPanel />
     <CommentMessageBox />
-    <PostActivityPanel />
+    <PostActivityPanel showPostRMModal={showPostRMModal} />
     <PostRemovalConfirmationModal />
   </Container>
 );
 
-export default PostViewer;
+PostViewer.propTypes = {
+  showPostRMModal: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = dispatch => ({
+  showPostRMModal: () => dispatch(showPostRemovalConfirmationModal())
+});
+
+export default connect(null, mapDispatchToProps)(PostViewer);
