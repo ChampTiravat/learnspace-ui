8133d5acbfe689dc2c4064225a91b3b0a2b7a8e6
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-01 10:57:39 +0700

    :hammer: Move Redux binding code from presentational component

--- core/components/PostPage/PostRemovalConfirmationModal.js
+++ core/components/PostPage/PostRemovalConfirmationModal.js
@@ -1,28 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
 
 import { ModalBackground, ModalPanel } from "..//Modal";
 import { PrimaryButton, LightButton } from "../Button";
 import { Header, Body, Footer } from "../Card";
-import {
-  showPostRemovalConfirmationModal,
-  hidePostRemovalConfirmationModal
-} from "../../actions/post-page-actions";
 
 /**
  * @name PostRemovalConfirmationModal
  * @desc A modal prompts for post removal confirmation
- * @prop isShowing : State of the modal(is displaying or not)
- * @prop showPostRMModal : Show the modal
+ * @prop isPostRMModalShowing : State of the modal(is displaying or not)
  * @prop hidePostRMModal : Hide the modal
  */
 const PostRemovalConfirmationModal = ({
-  isShowing,
-  showPostRMModal,
+  isPostRMModalShowing,
   hidePostRMModal
 }) => (
-  <ModalBackground show={isShowing}>
+  <ModalBackground show={isPostRMModalShowing}>
     <ModalPanel large>
       <Header>ยืนยันการดำเนินการ</Header>
       <Body style={{ color: "#777", fontSize: "1.2em", textAlign: "center" }}>
@@ -38,20 +31,8 @@ const PostRemovalConfirmationModal = ({
 );
 
 PostRemovalConfirmationModal.propTypes = {
-  isShowing: PropTypes.bool.isRequired,
-  showPostRMModal: PropTypes.func.isRequired,
+  isPostRMModalShowing: PropTypes.bool.isRequired,
   hidePostRMModal: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({
-  isShowing: state.postRemovalConfirmationModal
-});
-
-const mapDispatchToProps = dispatch => ({
-  showPostRMModal: () => dispatch(showPostRemovalConfirmationModal()),
-  hidePostRMModal: () => dispatch(hidePostRemovalConfirmationModal())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(
-  PostRemovalConfirmationModal
-);
+export default PostRemovalConfirmationModal;
--- core/components/PostPage/PostViewer.js
+++ core/components/PostPage/PostViewer.js
@@ -2,34 +2,54 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { showPostRemovalConfirmationModal } from "../../actions/post-page-actions";
 import PostRemovalConfirmationModal from "./PostRemovalConfirmationModal";
 import PostCommentsPanel from "./PostCommentsPanel";
 import CommentMessageBox from "./CommentMessageBox";
 import PostActivityPanel from "./PostActivityPanel";
 import PostContent from "./PostContent";
 import Container from "../Container";
+import {
+  hidePostRemovalConfirmationModal,
+  showPostRemovalConfirmationModal
+} from "../../actions/post-page-actions";
 
 /**
  * @name PostViewer
  * @desc Display post content, comments in one component
+ * @prop [REDUX] isPostRMModalShowing : State of the modal(is displaying or not)
+ * @prop [REDUX] showPostRMModal : Show the modal
+ * @prop [REDUX] hidePostRMModal : Hide the modal
  */
-const PostViewer = ({ showPostRMModal }) => (
+const PostViewer = ({
+  isPostRMModalShowing,
+  hidePostRMModal,
+  showPostRMModal
+}) => (
   <Container marginTop="11.5em">
     <PostContent />
     <PostCommentsPanel />
     <CommentMessageBox />
     <PostActivityPanel showPostRMModal={showPostRMModal} />
-    <PostRemovalConfirmationModal />
+    <PostRemovalConfirmationModal
+      isPostRMModalShowing={isPostRMModalShowing}
+      hidePostRMModal={hidePostRMModal}
+    />
   </Container>
 );
 
 PostViewer.propTypes = {
-  showPostRMModal: PropTypes.func.isRequired
+  isPostRMModalShowing: PropTypes.bool.isRequired,
+  showPostRMModal: PropTypes.func.isRequired,
+  hidePostRMModal: PropTypes.func.isRequired
 };
 
-const mapDispatchToProps = dispatch => ({
-  showPostRMModal: () => dispatch(showPostRemovalConfirmationModal())
+const mapStateToProps = state => ({
+  isPostRMModalShowing: state.postRemovalConfirmationModal
 });
 
-export default connect(null, mapDispatchToProps)(PostViewer);
+const mapDispatchToProps = dispatch => ({
+  showPostRMModal: () => dispatch(showPostRemovalConfirmationModal()),
+  hidePostRMModal: () => dispatch(hidePostRemovalConfirmationModal())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostViewer);
