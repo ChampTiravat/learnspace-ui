7c6b2dce5dee4f07c82d77189970ca3af84534d0
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-02 12:52:39 +0700

    :sparkles: :hammer: Added a correct data to <List />

--- core/components/PostEditor/PostComponents/EditableComponents/List.js
+++ core/components/PostEditor/PostComponents/EditableComponents/List.js
@@ -1,43 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-import { reduxForm, Field } from "redux-form";
 
 import { LIST } from "../../../../constants/post-content/components";
-import { Form, InputField, TextAreaField } from "../../../Form";
+import { Form, InputGroup, Input, InputLabel } from "../../../Form";
 import { CircleButton, Button } from "../../../Button";
 import { Header, Body, Footer } from "../../../Card";
-
-const ListItem = styled.li`
-  animation: ${p => p.theme.SHOW_FROM_TOP} 400ms;
-  position: relative;
-  width: 90%;
-`;
 
 /**
  * @name List
  * @desc Display a list with ordered items
- * @prop [REDUX-FORM] handleSubmit : Redux-Form's default form handle function
  * @prop hideAddPostComponentModal : f() to close the AddPostComponentModal(close the modal intentionally)
  * @prop addNewPostComponent : f() to append a new post component to receipe
  * @prop order : The component's current order in receipe
- * @prop type : Type of the component to be inserte
  */
 class List extends React.Component {
   state = {
-    items: ["first"]
+    description: "",
+    itemToAdd: "",
+    items: []
   };
 
   /**
    * @name submitHandler
    * @desc Append the <List /> component to the 'receipe'
    * @param { description } : Describe what is presenting in the list
-   * @param { items[string] } : Array of list items
+   * @param { items } : Array of list items
    */
   submitHandler = values => {
-    const { metadata, description } = values;
-    const items = Object.values(values).filter((item, i) => i !== 0);
-    const data = { description, items };
+    const { items, description } = this.state;
 
     const {
       hideAddPostComponentModal,
@@ -55,63 +45,93 @@ class List extends React.Component {
 
     addNewPostComponent({
       type: LIST,
-      order,
-      data
+      data: { description, items: items.map(item => item.data) },
+      order
     });
 
     hideAddPostComponentModal();
   };
 
-  removeItem = itemToDelete =>
+  /**
+   * @name removeItem
+   * @desc Remove one item from the list
+   * @param itemToDelete : A list item to remove
+   */
+  removeItem = itemToRemove =>
     this.setState(({ items }) => ({
-      items: items.filter(item => item !== itemToDelete)
+      items: items.filter(item => item.id !== itemToRemove.id)
     }));
 
-  addItem = () =>
+  /**
+   * @name addItem
+   * @desc Add one item to the list
+   */
+  addItem = () => {
     this.setState(({ items }) => ({
-      items: items.concat(
-        Math.ceil((Math.random() + (1 + Math.random())) * 100)
-      )
+      items: items.concat({
+        id: Math.ceil((Math.random() + (1 + Math.random())) * 100),
+        data: this.state.itemToAdd
+      })
     }));
+    this.setState({
+      itemToAdd: ""
+    });
+  };
 
-  renderItemDependsOnTotalItemsNumber = () =>
-    this.state.items.map((item, i) => (
-      <ListItem>
-        <Field
-          key={item}
-          name={`list-item-${item}`}
-          component={TextAreaField}
-        />
-        {item !== "first" ? (
-          <CircleButton
-            danger
-            type="button"
-            onClick={() => this.removeItem(item)}
-          >
-            -
-          </CircleButton>
-        ) : null}
-      </ListItem>
+  /**
+   * @name renderItems
+   * @desc Render every items which have been added to the list with remove button
+   */
+  renderItems = () =>
+    this.state.items.map(item => (
+      <li
+        key={item.id}
+        style={{
+          position: "relative",
+          width: "90%",
+          padding: "1.5em"
+        }}
+      >
+        {item.data}
+        <CircleButton
+          danger
+          type="button"
+          position="absolute"
+          top="1em"
+          right="-4em"
+          onClick={() => this.removeItem(item)}
+        >
+          -
+        </CircleButton>
+      </li>
     ));
 
   render() {
-    const { hideAddPostComponentModal, handleSubmit } = this.props;
+    const { hideAddPostComponentModal } = this.props;
     return [
       <Header>รายการ</Header>,
-      <Form onSubmit={handleSubmit(this.submitHandler)}>
+      <Form onSubmit={e => e.preventDefault() & this.submitHandler()}>
         <Body>
-          <Field
-            name="description"
-            label="คำอธิบายเกี่ยวกับรายการนี้"
-            component={InputField}
-            type="text"
-          />
+          <InputGroup>
+            <InputLabel>รายการนี้เกี่ยวข้องกับอะไร</InputLabel>
+            <Input
+              type="text"
+              name="description"
+              value={this.state.description}
+              onChange={e => this.setState({ description: e.target.value })}
+            />
+          </InputGroup>
           <ol>
-            {this.renderItemDependsOnTotalItemsNumber()}
+            {this.renderItems()}
+            <Input
+              name="itemToAdd"
+              value={this.state.itemToAdd}
+              onChange={e => this.setState({ itemToAdd: e.target.value })}
+            />
             <Button
               primary
-              type="button"
               textCenter
+              type="button"
               marginTop="1em"
               style={{ width: "90%" }}
               onClick={this.addItem}
@@ -121,10 +141,10 @@ class List extends React.Component {
           </ol>
         </Body>
         <Footer>
-          <Button success marginRight="0.5em">
+          <Button primary marginRight="0.5em">
             เสร็จสิ้น
           </Button>
-          <Button danger type="button" onClick={hideAddPostComponentModal}>
+          <Button light type="button" onClick={hideAddPostComponentModal}>
             ยกเลิก
           </Button>
         </Footer>
@@ -135,9 +155,8 @@ class List extends React.Component {
 
 List.propTypes = {
   hideAddPostComponentModal: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
   addNewPostComponent: PropTypes.func.isRequired,
   order: PropTypes.number.isRequired
 };
 
-export default reduxForm({ form: "list_component_data" })(List);
+export default List;
