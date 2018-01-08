1838501491adc7bdb3d40a9df0e7a04d72ef3459
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-08 17:28:38 +0700

    :zap: Created a component to edit an existing post

--- pages/edit_post.js
+++ pages/edit_post.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import AuthenticatedLayout from "../core/components/Layout/AuthenticatedLayout";
-import PostEditor from "../core/components/PostEditor/PostEditor";
+import EditPostEditor from "../core/components/EditPostPage/EditPostEditor";
 import Container from "../core/components/Container";
 import withData from "../core/withData";
 
 const EditPostPage = () => (
   <AuthenticatedLayout>
     <Container>
-      <PostEditor />
+      <EditPostEditor />
     </Container>
   </AuthenticatedLayout>
 );
