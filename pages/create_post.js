567774e217bd0add02e952a00e5ddc83b06e4097
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-08 11:07:19 +0700

    :fire: Removed useless import statement

--- pages/create_post.js
+++ pages/create_post.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import PostComponentsSelectorModal from "../core/components/PostEditor/PostComponentsSelectorModal";
 import AuthenticatedLayout from "../core/components/Layout/AuthenticatedLayout";
 import PostEditor from "../core/components/PostEditor/PostEditor";
 import Container from "../core/components/Container";
