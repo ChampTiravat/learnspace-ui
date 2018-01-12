283922bbf47b0ea6793354395820d882f63b46b6
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-13 03:13:00 +0700

    :zap: Sending accessToken to API

---
+++ core/constants/endpoints/api.js
@@ -0,0 +1,8 @@
+// API Endpoint
+export const API_ENDPOINT = `http://localhost:5000`
+
+// GraphQL Endpoint
+export const GRAPHQL_ENDPOINT = `${API_ENDPOINT}/graphql`
+
+// GraphQL Subscription(WebSocket) Endpoint
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = `ws://localhost:5000/subscriptions`
--- core/initApollo.js
+++ core/initApollo.js
@@ -1,11 +1,17 @@
-import { ApolloClient } from 'apollo-client'
-import { setContext } from 'apollo-link-context'
-import { createHttpLink } from 'apollo-link-http'
-import { WebSocketLink } from 'apollo-link-ws'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { getMainDefinition } from 'apollo-utilities'
-import { split } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { WebSocketLink } from 'apollo-link-ws'
+import { ApolloClient } from 'apollo-client'
 import fetch from 'isomorphic-fetch'
+import { split } from 'apollo-link'
+
+import { ACCESS_TOKEN } from './constants/security'
+import {
+  GRAPHQL_SUBSCRIPTION_ENDPOINT,
+  GRAPHQL_ENDPOINT
+} from './constants/endpoints/api'
 
 let apolloClient = null
 
@@ -13,71 +19,71 @@ let apolloClient = null
 // If this code executes on the server, using pluged in 'fetch'
 // ===================================================================
 if (!process.browser) {
-	global.fetch = fetch
+  global.fetch = fetch
 }
 
 // ===================================================================
 // Constructing a Apollo Network Interface
 // ===================================================================
 const httpLink = createHttpLink({
-	uri: 'http://localhost:5000/graphql', // Server URL (must be absolute)
-	credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
+  uri: GRAPHQL_ENDPOINT, // Server URL (must be absolute)
+  credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
 })
 
 // ===================================================================
 // Applying Apollo-client middleware(s)
 // ===================================================================
 const middlewareLink = setContext(() => ({
-	headers: {
-		authorization: localStorage.getItem('token') || ''
-	}
+  headers: {
+    authorization: localStorage.getItem(ACCESS_TOKEN) || ''
+  }
 }))
 
 let graphqlConnectionLink = null
 
 if (process.browser) {
-	// If the this code executes on the client, then attach WebSocket
-	graphqlConnectionLink = split(
-		({ query }) => {
-			const { kind, operation } = getMainDefinition(query)
-			return kind === 'OperationDefinition' && operation === 'subscription'
-		},
-		new WebSocketLink({
-			uri: `ws://localhost:5000/subscriptions`,
-			options: {
-				reconnect: true
-			}
-		}),
-		middlewareLink.concat(httpLink)
-	)
+  // If the this code executes on the client, then attach WebSocket
+  graphqlConnectionLink = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    new WebSocketLink({
+      uri: GRAPHQL_SUBSCRIPTION_ENDPOINT,
+      options: {
+        reconnect: true
+      }
+    }),
+    middlewareLink.concat(httpLink)
+  )
 } else {
-	// If the this code executes on the server, don't attach WebSocket
-	graphqlConnectionLink = middlewareLink.concat(httpLink)
+  // If the this code executes on the server, don't attach WebSocket
+  graphqlConnectionLink = middlewareLink.concat(httpLink)
 }
 
 const create = () => {
-	return new ApolloClient({
-		ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-		link: graphqlConnectionLink,
-		cache: new InMemoryCache()
-	})
+  return new ApolloClient({
+    ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
+    link: graphqlConnectionLink,
+    cache: new InMemoryCache()
+  })
 }
 
 export default function initApollo() {
-	// ===================================================================
-	// Create a new client for every server-side request so that data
-	// isn't shared between connections (which would be bad)
-	// ===================================================================
-	if (!process.browser) {
-		return create()
-	}
+  // ===================================================================
+  // Create a new client for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  // ===================================================================
+  if (!process.browser) {
+    return create()
+  }
 
-	// ===================================================================
-	// Reuse client on the client-side
-	// ===================================================================
-	if (!apolloClient) {
-		apolloClient = create()
-	}
+  // ===================================================================
+  // Reuse client on the client-side
+  // ===================================================================
+  if (!apolloClient) {
+    apolloClient = create()
+  }
 
-	return apolloClient
+  return apolloClient
 }
