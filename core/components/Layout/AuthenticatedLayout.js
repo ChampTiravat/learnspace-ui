a706b2d5955bb77e0a5b0be255f1a1e71beb572e
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-01-18 22:33:58 +0700

    :sparkles: :hammer: Get the current user's ID and send it through URL to fetch a user in profile page

--- core/components/Layout/AuthenticatedLayout.js
+++ core/components/Layout/AuthenticatedLayout.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import styled, { ThemeProvider } from 'styled-components'
 
+import { showNotificationModal } from '../../actions/notification-modal-actions'
+import { showChatroomModal } from '../../actions/chatroom-modal-actions'
 import UnAuthenticatedNav from '../NavigationBar/UnAuthenticatedNav'
 import AuthenticatedNav from '../NavigationBar/AuthenticatedNav'
 import ChatroomModal from '../ChatroomModal/ChatroomModal'
@@ -11,9 +15,18 @@ import Container from '../Container'
 import Card from '../Card'
 
 /**
+ * @name AuthenticatedLayout
  * @desc Use this Layout when user is authorized
+ * @prop { activeUser } [REDUX] : Currently logged-in user
+ * @prop { showChatroomModal } [REDUX] : Display a ChatroomModal
+ * @prop { showNotificationModal } [REDUX] : Display a NotificationModal
  */
-const AuthenticatedLayout = ({ children }) => (
+const AuthenticatedLayout = ({
+  children,
+  activeUser,
+  showChatroomModal,
+  showNotificationModal
+}) => (
   <ThemeProvider theme={defaultTheme}>
     {!validateToken() ? (
       <Container>
@@ -30,7 +43,11 @@ const AuthenticatedLayout = ({ children }) => (
       </Container>
     ) : (
       <Container>
-        <AuthenticatedNav />
+        <AuthenticatedNav
+          activeUser={activeUser}
+          showChatroomModal={showChatroomModal}
+          showNotificationModal={showNotificationModal}
+        />
         <NotificationModal />
         <ChatroomModal />
         {children}
@@ -39,4 +56,19 @@ const AuthenticatedLayout = ({ children }) => (
   </ThemeProvider>
 )
 
-export default AuthenticatedLayout
+AuthenticatedLayout.propTypes = {
+  showNotificationModal: PropTypes.func.isRequired,
+  showChatroomModal: PropTypes.func.isRequired,
+  activeUser: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  activeUser: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+  showNotificationModal: () => dispatch(showNotificationModal()),
+  showChatroomModal: () => dispatch(showChatroomModal())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticatedLayout)
