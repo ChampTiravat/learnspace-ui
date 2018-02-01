e7df1bab89c6cece67cca6f5c56b9d97244f58ac
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-02-02 00:41:22 +0700

    :sparkles: Querying actual post content

--- pages/post.js
+++ pages/post.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Head from "next/head";
-import Link from "next/link";
-import styled from "styled-components";
-import { connect } from "react-redux";
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import styled from 'styled-components'
 
-import PostRemovalConfirmationModal from "../core/components/PostPage/PostRemovalConfirmationModal";
-import ClassroomHeaderPanel from "../core/components/ClassroomPage/ClassroomHeaderPanel";
-import AuthenticatedLayout from "../core/components/Layout/AuthenticatedLayout";
-import PostViewer from "../core/components/PostPage/PostViewer";
-import withData from "../core/withData";
+import PostRemovalConfirmationModal from '../core/components/PostPage/PostRemovalConfirmationModal'
+import ClassroomHeaderPanel from '../core/components/ClassroomPage/ClassroomHeaderPanel'
+import AuthenticatedLayout from '../core/components/Layout/AuthenticatedLayout'
+import PostViewer from '../core/components/PostPage/PostViewer'
+import withData from '../core/withData'
 
 class PostPage extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ query: { id } }) {
     return {
-      classroomID: "1",
-      classroomName: "Introduction to Computer Science"
-    };
+      postID: id,
+      classroomID: '1',
+      classroomName: 'Introduction to Computer Science'
+    }
   }
 
   render() {
-    const { classroomID, classroomName } = this.props;
+    const { postID, classroomID, classroomName } = this.props
     return [
       <Head>
         <title>Introduction to Artificial Intelligence | LEARNSPACE</title>
@@ -29,10 +29,10 @@ class PostPage extends React.Component {
           classroomID={classroomID}
           classroomName={classroomName}
         />
-        <PostViewer />
+        <PostViewer postID={postID} />
       </AuthenticatedLayout>
-    ];
+    ]
   }
 }
 
-export default withData(PostPage);
+export default withData(PostPage)
