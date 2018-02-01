bd34d56cb033064fee0e4dc3d0bdaf2e9f5b39a8
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-02-01 22:53:15 +0700

    :lipstick: Recreated LoadingIcon

--- core/components/LoadingIcon.js
+++ core/components/LoadingIcon.js
@@ -1,23 +1,42 @@
 import styled, { keyframes } from 'styled-components'
 
-const BORDER_FADE_ANIMATION = keyframes`
-    from { border: 2px solid ${p => p.theme.PRIMARY_COLOR}; }
-      to { border: 2px solid #fff; }
+const spinningAnimation = keyframes`
+    0% { transform: rotate(0deg) scale(1); }
+   50% { transform: rotate(45deg) scale(1.1); }
+  100% { transform: rotate(360deg) scale(1); }
 `
+
 const LoadingSpinner = styled.div`
-  width: 170px;
-  padding: 1.3em;
-  margin: 5em auto;
-  border-radius: 15px;
-  border: 2px solid #fff;
-  box-shadow: 0 5px 50px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
+  box-shadow: 0 15px 60px rgba(0, 0, 0, 0.2);
   color: ${p => p.theme.PRIMARY_COLOR};
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 50%;
+  position: relative;
+  margin: 5em auto;
+  background: #fff;
   font-size: 1.4em;
-  font-weight: 500;
-  text-align: center;
-  animation: ${BORDER_FADE_ANIMATION} 300ms infinite;
+  display: flex;
+  height: 150px;
+  width: 150px;
+  &:before,
+  &:after {
+    top: -10px;
+    left: -10px;
+    content: '';
+    width: 150px;
+    height: 150px;
+    display: block;
+    position: absolute;
+    border-radius: 50%;
+    border: 10px solid transparent;
+    border-top-color: ${p => p.theme.PRIMARY_COLOR};
+    border-bottom-color: ${p => p.theme.PRIMARY_COLOR};
+    animation: ${spinningAnimation} 800ms linear infinite;
+  }
 `
+
 /**
  * @name LoadingIcon
  * @desc Display loading icon when the page is loading
