7dd1ce51ef8f10ee77082b3cc9197c7f1f386f61
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2018-02-03 00:41:54 +0700

    :sparkles: :zap: lifted GraphQL Operation to the page component and display post's comments based on the real data

--- core/components/PostPage/PostCommentsPanel.js
+++ core/components/PostPage/PostCommentsPanel.js
@@ -1,75 +1,81 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-import Card, { Header, Body, Footer } from "../Card";
-import PostCommentItem from "./PostCommentItem";
+import Card, { Header, Body, Footer } from '../Card'
+import PostCommentItem from './PostCommentItem'
 
 const SubCommentsWrapper = styled.ul`
-  list-style: none;
   padding-left: 5em;
-`;
+  list-style: none;
+`
 
 const CommentPanelCard = Card.extend`
-  max-width: 100%;
-  width: 900px;
-  height: 700px;
   padding-bottom: 4em;
   margin-bottom: 2em;
-`;
+  max-width: 100%;
+  height: 500px;
+  width: 900px;
+`
 
 /**
  * @name Comment
  * @desc Display user comment with it's sub-comment(s)
- * @prop creator : Comment's creator
- * @prop content : Comment message body
+ * @prop { creator } [APOLLO] : Comment's creator
+ * @prop { content } [APOLLO] : Comment message body
+ * @prop { subComments } [APOLLO] : Comment's sub-comments
  */
-const Comment = ({ creator, content }) => (
+const Comment = ({ creator, content, subComments }) => (
   <div>
     <PostCommentItem creator={creator} content={content} />
     <SubCommentsWrapper>
-      <PostCommentItem isReply creator={creator} content={content} />
-      <PostCommentItem isReply creator={creator} content={content} />
-      <PostCommentItem isReply creator={creator} content={content} />
+      {subComments.length != 0
+        ? subComments.map((comment, i) => (
+            <PostCommentItem
+              isReply
+              content={comment.message}
+              creator={comment.creator.fname}
+            />
+          ))
+        : null}
     </SubCommentsWrapper>
   </div>
-);
+)
 
 Comment.propTypes = {
   creator: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired
-};
+  content: PropTypes.string.isRequired,
+  subComments: PropTypes.array
+}
 
 /**
  * @name PostCommentsPanel
  * @desc Display multiple comments of a particular post
+ * @prop { comments } [REDUX] : List of comments
  */
-const PostCommentsPanel = () => (
+const PostCommentsPanel = ({ comments }) => (
   <CommentPanelCard>
     <Header>ความคิดเห็น</Header>
-    <Body overflowY="scroll" height="600px">
-      <Comment
-        creator="Tony Stark"
-        content="ไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเ"
-      />
-      <Comment
-        creator="Steve Rogers"
-        content="ไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเ"
-      />
-      <Comment
-        creator="Chris Evans"
-        content="ไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเ"
-      />
-      <Comment
-        creator="Tony Stark"
-        content="ไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเ"
-      />
-      <Comment
-        creator="Chris Evans"
-        content="ไสดมวไำดยสเนาฟบไำานดำฟสาดร่เพเาๆขนายำสกยบากนฟกานฟหากจๆไำาดชๆสยไำนด่ำไจาเ"
-      />
+    <Body overflowY="auto" height="450px">
+      {!comments || comments.length == 0 ? (
+        <Card padding="2em 4em" marginTop="3em" textCenter>
+          <h3>ยังไม่มีความคิดเห็นใดๆ</h3>
+        </Card>
+      ) : (
+        comments.map((comment, i) => (
+          <Comment
+            content={comment.message}
+            creator={comment.creator.fname}
+            subComments={comment.subComments}
+          />
+        ))
+      )}
     </Body>
   </CommentPanelCard>
-);
+)
 
-export default PostCommentsPanel;
+PostCommentsPanel.propTyps = {
+  comments: PropTypes.array.isRequired
+}
+
+export default PostCommentsPanel
