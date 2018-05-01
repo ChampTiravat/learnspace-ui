a418f7c9e4423a7a8b5c7078ce847942da53f233
tiravat <tiravat2016@gmail.com>
2018-05-01 22:19:01 +0700

    :lipstick: Adjusted components' margin to match new design of ClassroomHeaderPanel

--- pages/chatroom.js
+++ pages/chatroom.js
@@ -1,28 +1,28 @@
-import React from "react";
-import Head from "next/head";
-import Router from "next/router";
+import React from 'react'
+import Head from 'next/head'
+import Router from 'next/router'
 
-import UserConnectionStatusPanel from "../core/components/ChatroomPage/UserConnectionStatusPanel";
-import ClassroomActivityPanel from "../core/components/ClassroomPage/ClassroomActivityPanel";
-import ClassroomHeaderPanel from "../core/components/ClassroomPage/ClassroomHeaderPanel";
-import ChatMessagesPanel from "../core/components/ChatroomPage/ChatMessagesPanel";
-import AuthenticatedLayout from "../core/components/Layout/AuthenticatedLayout";
-import { CLASSROOM_PAGE } from "../core/constants/endpoints/ui";
-import Container from "../core/components/Container";
-import Panel from "../core/components/Panel";
-import withData from "../core/withData";
+import UserConnectionStatusPanel from '../core/components/ChatroomPage/UserConnectionStatusPanel'
+import ClassroomActivityPanel from '../core/components/ClassroomPage/ClassroomActivityPanel'
+import ClassroomHeaderPanel from '../core/components/ClassroomPage/ClassroomHeaderPanel'
+import ChatMessagesPanel from '../core/components/ChatroomPage/ChatMessagesPanel'
+import AuthenticatedLayout from '../core/components/Layout/AuthenticatedLayout'
+import { CLASSROOM_PAGE } from '../core/constants/endpoints/ui'
+import Container from '../core/components/Container'
+import Panel from '../core/components/Panel'
+import withData from '../core/withData'
 
 class ChatroomPage extends React.Component {
   static async getInitialProps() {
     return {
       isMember: 1,
-      classroomID: "1",
-      classroomName: "Introduction to Computer Science"
-    };
+      classroomID: '1',
+      classroomName: 'Introduction to Computer Science'
+    }
   }
 
   render() {
-    const { isMember, classroomID, classroomName } = this.props;
+    const { isMember, classroomID, classroomName } = this.props
     return !isMember
       ? Router.push(CLASSROOM_PAGE)
       : [
@@ -30,11 +30,8 @@ class ChatroomPage extends React.Component {
             <title>ห้องแชท - {classroomName} | LEARNSPACE</title>
           </Head>,
           <AuthenticatedLayout>
-            <ClassroomHeaderPanel
-              classroomID={classroomID}
-              classroomName={classroomName}
-            />
-            <Container marginTop="12em">
+            <ClassroomHeaderPanel classroomID={classroomID} classroomName={classroomName} />
+            <Container marginTop="8em">
               <Panel left width="40" paddingBottom="6em">
                 <UserConnectionStatusPanel />
               </Panel>
@@ -44,8 +41,8 @@ class ChatroomPage extends React.Component {
             </Container>
             <ClassroomActivityPanel />
           </AuthenticatedLayout>
-        ];
+        ]
   }
 }
 
-export default withData(ChatroomPage);
+export default withData(ChatroomPage)
