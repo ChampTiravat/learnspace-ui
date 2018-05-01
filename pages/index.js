bf6ebaf65bedd2ad5b3da8bc6cb81f92fd4498e9
tiravat <tiravat2016@gmail.com>
2018-05-02 03:29:20 +0700

    :hammer: Using uni-component rendering instead of array rendering

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ import Head from 'next/head'
 import UnAuthenticatedLayout from '../core/components/Layout/UnAuthenticatedLayout'
 import withData from '../core/withData'
 
-const IndexPage = () => [
-  <Head>
-    <title>หน้าแรก | LEARNSPACE</title>
-  </Head>,
-  <UnAuthenticatedLayout>
-    <h1>Hello</h1>
-  </UnAuthenticatedLayout>
-]
+const IndexPage = () => (
+  <div>
+    <Head>
+      <title>หน้าแรก | LEARNSPACE</title>
+    </Head>
+    <UnAuthenticatedLayout>
+      <h1>Hello</h1>
+    </UnAuthenticatedLayout>
+  </div>
+)
 
 export default withData(IndexPage)
