5ec0cdb31dc763798c6412478f07741973495ba7
tiravat <tiravat2016@gmail.com>
2018-05-01 22:13:15 +0700

    :lipstick: Set default font color

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet, injectGlobal } from "styled-components";
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet, injectGlobal } from 'styled-components'
 
 // Normalize the global style
 injectGlobal`
@@ -19,19 +19,20 @@ injectGlobal`
         margin: 0;
         width: 100%;
         height: 100%;
+        color: #777;
         background-color: #f5f5f5;
     }
 
     h1,h2,h3,h4,h5,h6 { margin: 0; }
 
     a {text-decoration: none;}
-`;
+`
 
 class MyDocument extends Document {
   render() {
-    const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
-    const styleTags = sheet.getStyleElement();
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
 
     return (
       <html>
@@ -48,8 +49,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
