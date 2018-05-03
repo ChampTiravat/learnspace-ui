5f766fdd1c0bdedd3b8a97f48e05ca69ee48e1d1
tiravat <tiravat2016@gmail.com>
2018-05-03 22:17:36 +0700

    :art: Created new component to reduce code repeatition

--- core/components/ClassroomPage/ClassroomPreview.js
+++ core/components/ClassroomPage/ClassroomPreview.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-import { Button } from "../Button";
-import Card from "../Card";
+import { Button } from '../Button'
+import Card from '../Card'
 
 const ClassroomPreviewWrapper = Card.extend`
   text-align: center;
@@ -11,63 +11,59 @@ const ClassroomPreviewWrapper = Card.extend`
   margin-top: 1.5em;
   margin-bottom: 3em;
   padding: 4em 3em;
-`;
+`
 
 const PrimaryInfoItem = styled.div`
   margin-bottom: 2em;
-`;
+`
 
 const Topic = styled.h2`
   font-weight: 400;
   color: ${p => p.theme.PRIMARY_COLOR};
   font-size: 1.6em;
-`;
+`
 
 const Info = styled.h1`
   color: #777;
   font-size: 1.8em;
   font-weight: 400;
-`;
+`
 
 const TextInfo = styled.p`
   color: #777;
   margin: 0;
   font-size: 1.1em;
-`;
+`
+
+const ClassroomInfoItem = ({ topic, info }) => (
+  <PrimaryInfoItem>
+    <Topic>{topic}</Topic>
+    <Info>{info}</Info>
+  </PrimaryInfoItem>
+)
 
 /**
  * @name ClassroomPreview
  * @desc Show a short information about a classroom if user is not a member of it, instead of showing the classroom timeline
- * @prop classroomID : ID of the classroom. use this to determine which classroom to query from API
+ * @prop { classroomID } [APOLLO] : ID of the classroom. use this to determine which classroom to query from API
+ * @prop { subject } [APOLLO] : Classroom's primary subject
+ * @prop { instructor } [APOLLO] :  Classroom's instructor
+ * @prop { description } [APOLLO] :  Classroom's description
  */
-const ClassroomPreview = ({
-  classroomID,
-  subject,
-  instructor,
-  description
-}) => (
+const ClassroomPreview = ({ subject, instructor, description, classroomID }) => (
   <ClassroomPreviewWrapper>
-    <PrimaryInfoItem>
-      <Topic>วิชาที่สอน</Topic>
-      <Info>{subject}</Info>
-    </PrimaryInfoItem>
-    <PrimaryInfoItem>
-      <Topic>ผู้สอน</Topic>
-      <Info>{instructor}</Info>
-    </PrimaryInfoItem>
-    <PrimaryInfoItem>
-      <Topic>คำอธิบาย</Topic>
-      <TextInfo>{description}</TextInfo>
-    </PrimaryInfoItem>
+    <ClassroomInfoItem topic="วิชาที่สอน" info={subject} />
+    <ClassroomInfoItem topic="ผู้สอน" info={instructor} />
+    <ClassroomInfoItem topic="คำอธิบาย" info={description} />
     <Button primary>ส่งคำขอเข้าห้องเรียนนี้</Button>
   </ClassroomPreviewWrapper>
-);
+)
 
 ClassroomPreview.propTypes = {
   classroomID: PropTypes.string.isRequired,
   subject: PropTypes.string.isRequired,
   instructor: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired
-};
+}
 
-export default ClassroomPreview;
+export default ClassroomPreview
